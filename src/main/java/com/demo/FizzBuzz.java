ee01ca8e6053261cd3777bfd1504d30106fde1bb
NickHale1 <47830428+NickHale1@users.noreply.github.com>
2022-03-30 17:45:35 -0400

    Fixed output formatting
    output format was wrong passes tests now

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -8,13 +8,13 @@ public class FizzBuzz
 			return "Invalid";
 		}
 		if(num%15==0) {
-			return "Fizz Buzz";
+			return "FizzBuzz";
 		} else if (num%5==0) {
 			return "Buzz";
 		} else if (num%3==0) {
 			return "Fizz";
 		}
-		
+
 		return String.valueOf(num);
 	}
 	
@@ -39,6 +39,11 @@ public class FizzBuzz
 		}
 
 		return output;
+	}
+
+	public static void main(String[] args) {
+		FizzBuzz fb = new FizzBuzz();
+		System.out.println(fb.processNumbers(new int[] {1, 2, 3, 5, 6, 10, 15, 30}));
 	}
 
 }
